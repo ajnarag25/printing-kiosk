a376c83c3e057fc859c35c4781b2f3b7bbb628b1
Aj Narag <ajnarag25@gmail.com>
2023-03-10 00:13:52 +0800

    added trial function for uploading file

--- user_print.php
+++ user_print.php
@@ -23,8 +23,45 @@
             <img src="assets/icons/BACK.png" width="50" alt="">
         </a>
         <?php 
-            echo $_FILES["uploaded_file"]["name"];
+        if(isset($_FILES["uploaded_file"])) {
+            $targetDir = "uploads/";
+            $targetFile = $targetDir . basename($_FILES["uploaded_file"]["name"]);
+            $uploadOk = 1;
+            $fileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+        
+            // Check if file already exists
+            if (file_exists($targetFile)) {
+                echo "Sorry, file already exists.";
+                $uploadOk = 0;
+            }
+        
+            // Check file size
+            if ($_FILES["uploaded_file"]["size"] > 500000) {
+                echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+        
+            // Allow certain file formats
+            if($fileType != "jpg" && $fileType != "png" && $fileType != "jpeg"
+            && $fileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+        
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], $targetFile)) {
+                    echo "The file ". basename( $_FILES["uploaded_file"]["name"]). " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }
+        }
         ?>
+        
         <div class="text-center header-space">
             <img src="assets/icons/LOAD SOME PAPER.png" width="150" alt="">
             <h1 class="title-user">Load some paper</h1>
