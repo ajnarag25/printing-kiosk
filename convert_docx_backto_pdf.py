ca81cbab2ab9636c5630a4ce8c300a1f856f87a8
michaelrilan <micorilan1999@gmail.com>
2023-03-10 10:20:42 +0800

    removing/transfeerring raw files

--- convert_docx_backto_pdf.py
+++
@@ -1,4 +0,0 @@
-from docx2pdf import convert
-
-
-convert("new_muscles.docx", "outt.pdf")
--- printhings.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def command_print(event = None):
-    command = "{} {}".format('C:/xampp/htdocs/printing_kiosk/printing_kiosk/PDFtoPrinter.exe',"""C:/Users/admin/Downloads/COLORED_PAGE.pdf Brother-DCP-T500W-Printer copies=2""")
-    subprocess.call(command,shell=True)
-
-command_print()
