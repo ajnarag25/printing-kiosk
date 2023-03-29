d29f6c12fb308d491a50428580690c416af69495
Aj Narag <ajnarag25@gmail.com>
2023-03-29 11:39:51 +0800

    fixed some indentions and added forms in admin side

--- main_app_print/forms.py
+++ main_app_print/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from .models import *
 
+
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = UploadedFile
         fields = ['uploaded_file']
+
+
+class price(forms.ModelForm):
+    class Meta:
+        model = admin_price
+        fields = '__all__'
+
+
+class password(forms.ModelForm):
+    class Meta:
+        model = admin_password
+        fields = '__all__'
+
+
+class timer(forms.ModelForm):
+    class Meta:
+        model = admin_timer
+        fields = '__all__'
